74e39fbb596ab0c09d3fdd405c55dd3570fe3a7c
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-11-30 11:17:54 -0700

    change name to chatty chalkboard

--- src/Nav-bar.jsx
+++ src/Nav-bar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
+        <a href="/" className="navbar-brand">Chatty Chalkboard</a>
         <span className="userCount">{this.props.userCount} User(s) online</span>
       </nav>
     );
